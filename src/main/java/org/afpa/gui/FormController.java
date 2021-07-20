4d4478a4474696b235766d4270dc5769cfe89ffd
beneko <behzadnekouei@gmail.com>
2021-07-20 12:20:40 +0200

    additioneur tested

--- src/main/java/org/afpa/gui/FormController.java
+++ src/main/java/org/afpa/gui/FormController.java
@@ -1,4 +1,31 @@
-package org.afpa.gui;
+package org.afpa.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 
 public class FormController {
+
+    public Button zero;
+    public Button one;
+    public Button two;
+    public Button three;
+    public Button four;
+    public Button eight;
+    public Button seven;
+    public Button six;
+    public Button five;
+    public Button nine;
+    public Button vider;
+    public Button calculer;
+
+//    public void calculer(ActionEvent actionEvent) {
+//    }
+//
+//    public void vider(ActionEvent actionEvent) {
+//    }
+
+    public void addNum(ActionEvent actionEvent) {
+        Button num = (Button) actionEvent.getSource();
+        System.out.println(num);
+    }
 }
